c495f214085de63debba47daf1c241d2783497f8
Giordan-88 <giordan88@hotmail.com>
2025-03-04 21:26:47 -0300

    arreglo

--- Hito1/src/assets/components/Navbar.jsx
+++ Hito1/src/assets/components/Navbar.jsx
@@ -2,28 +2,30 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-
+import Button from "./Button";
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" >
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Pizzería Mamma Mia!</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">🍕 Home</Nav.Link>
-
-            <NavDropdown title="🔓 Profile" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">🔐 Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">🔒 Logout</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">📖History</NavDropdown.Item>
+            <Button href="#features" buttonText={"🍕 Home"} />
+            <NavDropdown title="💼 Account" id="collapsible-nav-dropdown">
+              <Button href="#action/3.1" buttonText={"🔓 Profile"} />
+              <Button href="#action/3.2" buttonText={"🔒 Logout"} />
+              <Button href="#action/3.3" buttonText={"📖History"} />
             </NavDropdown>
-            <Nav.Link href="#pricing">🔐 Register</Nav.Link>
+            <Button href="#pricing" buttonText={"🔐 Login"} />
+            <Button href="#pricing" buttonText={"🔐 Register"} />
           </Nav>
           <Nav>
-            <Nav.Link eventKey={2} href="#memes">
-              🛒 Total: $25.000
-            </Nav.Link>
+            <Button
+              eventKey={2}
+              href="#memes"
+              buttonText={"🛒 Total: $25.000"}
+            />
           </Nav>
           <Nav></Nav>
         </Navbar.Collapse>
